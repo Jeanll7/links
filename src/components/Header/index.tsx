1ff79d89b6f0462ee236156cfcb9473991f8e847
Jean leal <jeancarloleall@gmail.com>
2024-03-11 02:09:31 -0300

    admin

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,8 +1,40 @@
 
+import { BiLogOut } from 'react-icons/bi'
+import { Link } from 'react-router-dom'
+
+import { auth } from '../../services/firebaseConnection'
+import { signOut } from 'firebase/auth'
+
 export function Header() {
+
+  async function handleLogout() {
+    try {
+      await signOut(auth);
+      console.log('Logout bem sucedido');
+    } catch (error) {
+      console.error('Erro ao fazer logout:', error);
+    }
+  }
+
   return (
-    <header>
-      <h1>TESTE</h1>
+    <header className='w-full max-w-2xl mt-4 px-1'>
+      <nav className="w-full bg-white h-12 flex items-center justify-between rounded-md px-3">
+        <div className='flex gap-4 font-medium'>
+          <Link to="/">
+            Home
+          </Link>
+          <Link to="/admin">
+            Links
+          </Link>
+          <Link to="/admin/social">
+            Redes sociais
+          </Link>
+        </div>
+
+        <button onClick={handleLogout}>
+          <BiLogOut size={28} color='#db2629' />
+        </button>
+      </nav>
     </header>
   )
 }
