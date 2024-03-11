4d9bf6059650d3be9e27a8460eb2f0fb100ee18b
Jean leal <jeancarloleall@gmail.com>
2024-03-11 02:18:48 -0300

    input

--- src/pages/admin/index.tsx
+++ src/pages/admin/index.tsx
@@ -1,9 +1,17 @@
 import { Header } from "../../components/Header";
+import { Input } from "../../components/Input";
 
 export function Admin() {
   return (
     <div className="flex items-center flex-col min-h-screen pb-7 px-2">
       <Header />
+
+      <form className="flex flex-col mt-8 mb-3 w-full max-w-xl">
+        <label className="text-white font-medium mt-2 mb-2">Nome do Link</label>
+        <Input
+          placeholder="Digite o nome do link..."
+        />
+      </form>
     </div>
   )
 }
